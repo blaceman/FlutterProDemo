d2ae821db025f7c2b71d1807ee87f2dcc9a67d0e
blaceman <991646695@qq.com>
2023-10-19 23:05:14 +0800

    freezed 模型及用法

--- lib/freezed_models/content/content.dart
+++ lib/freezed_models/content/content.dart
@@ -27,3 +27,26 @@ class Content with _$Content {
   factory Content.fromJson(Map<String, dynamic> json) =>
       _$ContentFromJson(json);
 }
+
+/**步骤：
+ *1.https://app.quicktype.io/去生成dart的model代码，勾选@freezed 以及json_serializable
+ *2.新建文件把生成代码复制进去
+ *3.运行:dart run build_runner build
+ */
+//@freezed 注解替换为 @unfreezed ：定义不可变的类或者一个可变的类
+//@Freezed(makeCollectionsUnmodifiable: false)：允许对 List/Map/Set 进行修改
+//生成copyWith方法。这允许您轻松创建当前对象的深浅拷贝,并在必要时更改某些属性。
+//生成==/hashCode方法。这使您的类变成可哈希和可比较的,可以使用在Set和Map中，在不可变类。
+//生成toJson和fromJson方法。这使您的类可以轻松与JSON序列化和反序列化。
+  // var cont = Content(
+  //       id: 111,
+  //       name: 'name',
+  //       categoryName: 'categoryName',
+  //       coverPicFullName: 'coverPicFullName',
+  //       viewCount: 2,
+  //       favoriteCount: 3);
+  //   cont.name = '222';//定义@unfreezed
+  //   var cont2 = cont.copyWith(name: '333');//copyWith方法
+  //   print(cont == cont2); // hashCode方法
+  //   var cont3 = Content.fromJson({});//序列化
+  //   cont3.toJson();//反序列化
